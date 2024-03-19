e04b1219044d9f124a2a570021735cf9212b9620
Luis Angel Mendoza Chate <49343533+lmendoza27@users.noreply.github.com>
2024-03-18 21:42:25 -0500

    changes

--- frontend/src/app/login.service.ts
+++ frontend/src/app/login.service.ts
@@ -7,9 +7,18 @@ import { Observable } from 'rxjs';
 })
 export class LoginService {
 
+  private serverUrl = 'http://localhost:8081';
+  // private serverUrl = 'https://310f-2001-1388-ae0-7600-5dc6-1450-162b-9748.ngrok-free.app';
+
   constructor(private http: HttpClient) { }
 
   login(credentials: any): Observable<any> {
-    return this.http.post<any>('http://localhost:8081/login', credentials);
+    //return this.http.post<any>('http://localhost:8081/login', credentials);
+    return this.http.post<any>(`${this.serverUrl}/login`, credentials);
   }
+
+  setServerUrl(url: string) {
+    this.serverUrl = url; // Método para establecer la URL del servidor
+  }
+
 }
--- frontend/src/app/product.service.ts
+++ frontend/src/app/product.service.ts
@@ -7,40 +7,43 @@ import { Observable } from 'rxjs';
 })
 export class ProductService {
 
+  private serverUrl = 'http://localhost:8081';
+  // private serverUrl = 'https://310f-2001-1388-ae0-7600-5dc6-1450-162b-9748.ngrok-free.app';
+
   constructor(private http: HttpClient) { }
 
   getProducts(): Observable<any[]> {
-    return this.http.get<any[]>('http://localhost:8081/productos');
+    return this.http.get<any[]>(`${this.serverUrl}/productos`);
   }
 
   getListProducts(): Observable<any[]> {
-    return this.http.get<any[]>('http://localhost:8081/detail_product');
+    return this.http.get<any[]>(`${this.serverUrl}/detail_product`);
   }
 
   saveProduct(productData: FormData): Observable<any> {
     // Realizar una solicitud HTTP POST al servidor para guardar el producto
-    return this.http.post<any>('http://localhost:8081/save_product', productData);
+    return this.http.post<any>(`${this.serverUrl}/save_product`, productData);
   }
 
   deleteProduct(productId: number): Observable<any> {
     // Realizar una solicitud HTTP DELETE al servidor para eliminar el producto
-    return this.http.delete<any>(`http://localhost:8081/producto/${productId}`);
+    return this.http.delete<any>(`${this.serverUrl}/producto/${productId}`);
   }
 
   getProductById(productId: number): Observable<any> {
-    return this.http.get<any>(`http://localhost:8081/producto/${productId}`);
+    return this.http.get<any>(`${this.serverUrl}/producto/${productId}`);
   }
 
   updateProduct(formData: FormData): Observable<any> {
-    return this.http.post<any>('http://localhost:8081/update_product', formData);
+    return this.http.post<any>(`${this.serverUrl}/update_product`, formData);
   }
 
   exportarExcel(): Observable<any> {
-    return this.http.get('http://localhost:8081/download_excel', { responseType: 'arraybuffer' });
+    return this.http.get(`${this.serverUrl}/download_excel`, { responseType: 'arraybuffer' });
   }
 
   downloadTechnicalSheet(id: number): Observable<Blob> {
-    return this.http.get(`http://localhost:8081/ficha_tecnica/${id}`, { responseType: 'blob' });
+    return this.http.get(`${this.serverUrl}/ficha_tecnica/${id}`, { responseType: 'blob' });
   }
 
   updateProductPrice(idProducto: number, precioVenta: number, correo: string) {
@@ -50,14 +53,18 @@ export class ProductService {
       toPerson: correo
     };
 
-    return this.http.post<any>('http://localhost:8081/quote_product', body);
+    return this.http.post<any>(`${this.serverUrl}/quote_product`, body);
   }
 
   uploadProductsExcel(file: File) {
     const formData = new FormData();
     formData.append('file', file);
 
-    return this.http.post<any>('http://localhost:8081/upload_products_excel', formData);
+    return this.http.post<any>(`${this.serverUrl}/upload_products_excel`, formData);
+  }
+
+  setServerUrl(url: string) {
+    this.serverUrl = url; // Método para establecer la URL del servidor
   }
 
 }
