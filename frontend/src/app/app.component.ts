c53dfbc1f08b09a3c506abf2921fe9756c3ba617
Luis Angel Mendoza Chate <49343533+lmendoza27@users.noreply.github.com>
2024-03-18 23:10:01 -0500

    oka

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'frontenddd';
+
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    this.router.navigate(['/login']); // Redirige automáticamente a la página de inicio de sesión al iniciar la aplicación
+  }
+
 }
